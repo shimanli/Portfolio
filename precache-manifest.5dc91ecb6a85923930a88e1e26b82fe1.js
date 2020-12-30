e981836e10d82a871dcd8626ec1db4e615fffaa3
Yiyang Song <yiyangsong@Yiyangs-Air.fios-router.home>
2020-12-30 09:54:43 -0500

    Updates

---
+++ precache-manifest.5dc91ecb6a85923930a88e1e26b82fe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a97f41886873c5ef44d9b69b337278",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3df00a479e5d1ee6a7f4",
+    "url": "/Portfolio/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/Portfolio/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Portfolio/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df00a479e5d1ee6a7f4",
+    "url": "/Portfolio/static/js/main.1e2f6057.chunk.js"
+  },
+  {
+    "revision": "87bcb24a22a073d38476",
+    "url": "/Portfolio/static/js/runtime-main.d4446722.js"
+  },
+  {
+    "revision": "1896619d42bb7473760552282e183742",
+    "url": "/Portfolio/static/media/resume.1896619d.pdf"
+  },
+  {
+    "revision": "a5b5a7e302d5ce89f23b05165e180911",
+    "url": "/Portfolio/static/media/shimanli.a5b5a7e3.png"
+  }
+]);
